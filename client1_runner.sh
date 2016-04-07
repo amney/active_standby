7a9809c1b0eb1348f750484983881fcc8e98b566
Corne Cornelius <ccornelius@10.182.73.21>
2016-04-07 15:11:40 -0400

    - updated client1_runner.sh to only run single instance

--- client1_runner.sh
+++ client1_runner.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# When running on APIC this may be needed
 export PYTHONPATH=$PYTHONPATH:/home/admin/.local/lib/python2.7/site-packages/
 
 # get ACT/SBY from client1.env
@@ -7,6 +8,27 @@ source ./client1.env
 
 # base64 decode APIC password
 APICPASS=$(base64 -d<<<"${APICPASS64}")
+
+# Only allow single instance
+PIDFILE=/var/run/apic-mon.key-client1.pid
+
+# Check if another instance is already running
+if [ -r "${PIDFILE}" ];then
+	MYPID=$(cat ${PIDFILE})
+	if (pgrep -U $MYPID apic-mon.key >/dev/null 2>&1);then
+		exit "$0 already running, only 1 instance allowed"
+	fi
+fi
+
+[ -r "${PIDFILE}" ] || [ ! -f "${PIDFILE}" ] && echo $$ > ${PIDFILE} 
+[ ! -r "${PIDFILE}" ] && echo "Cant create pid file: ${PIDFILE}" && exit
+
+function cleanup() {
+	[ -r "${PIDFILE}" ] && rm -f "${PIDFILE}"
+}
+
+trap cleanup EXIT
+
 
 # loop forever and swap active/standby in the loop
 while (true); do
